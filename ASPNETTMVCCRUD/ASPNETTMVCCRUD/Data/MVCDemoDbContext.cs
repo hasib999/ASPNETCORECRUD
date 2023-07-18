abe6ae3721f5671b9002c5adc29a5a08ae41d080
Md. Hasibul Islam <45915540+hasib999@users.noreply.github.com>
2023-07-19 00:42:48 +0600

    Package Added
    EntityFrameWork.SqlServer & Tools

---
+++ ASPNETTMVCCRUD/ASPNETTMVCCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETTMVCCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
